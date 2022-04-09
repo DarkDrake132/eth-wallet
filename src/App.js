52a048ff7032725dae7e86e1ba7d3510126bbaee
Bach <50036032+DarkDrake132@users.noreply.github.com>
2022-04-09 19:51:34 +0700

    setup first look

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { generateMnemonic } from "bip39";
+import { Wallet, providers } from "ethers";
+import "./App.css";
 
 function App() {
+  const [mnemonic, setMnemonic] = useState("");
+  const [mnemonicInput, setMnemonicInput] = useState("");
+  const [wallet, setWallet] = useState()
+  const [balance, setBalance] = useState(0)
+  const [transactions, setTransactions] = useState({
+    to: "",
+    
+  })
+
+  useEffect(() => {
+    window.ethersProvider = new providers.InfuraProvider("rinkeby")
+  }, [])
+
+  useEffect(() => {
+    if (wallet) {
+      window.ethersProvider.getBalance(wallet.address).then(balance => {
+        setBalance(balance)
+      })
+    }
+  }, [wallet])
+
+  function genreateNewMnemonic() {
+    setMnemonic(generateMnemonic());
+  }
+
+  function createWalletFromMnemonic() {
+    let walletCreate = Wallet.fromMnemonic(mnemonic ? mnemonic : mnemonicInput);
+    walletCreate = {...walletCreate, provider: window.ethersProvider}
+    setWallet(walletCreate)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Generate Mnemonic for Wallet</h3>
+      <button onClick={genreateNewMnemonic}>Generate</button>
+      {mnemonic ? (
+        <div>
+          <div>Your Mnemonic: {mnemonic}</div>
+        </div>
+      ) : null}
+
+      <p>Or import Wallet using Mnemonic</p>
+      <input value={mnemonicInput} onChange={(e) => setMnemonicInput(e.target.value)} />
+
+      <button onClick={createWalletFromMnemonic}>Create Wallet</button>
+      <div>Your wallet information</div>
+      {wallet ? (
+        <div>
+          <h4>Wallet Address: {wallet.address}</h4>
+          <p>Balance: {balance.toString()}</p>
+          <div>
+            <h3>Send Transaction</h3>
+            <p>To: </p>
+            <input />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
