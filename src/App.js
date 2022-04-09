c756a72e533d64685c172c7f0faa8fc4729bc33b
Bach <50036032+DarkDrake132@users.noreply.github.com>
2022-04-09 22:45:33 +0700

    wallet full function
    create wallet from mnemonic, send tx, get balance

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import { useState, useEffect } from "react";
 import { generateMnemonic } from "bip39";
-import { Wallet, providers } from "ethers";
+import { Wallet, providers, utils } from "ethers";
 import "./App.css";
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
   const [mnemonicInput, setMnemonicInput] = useState("");
-  const [wallet, setWallet] = useState()
-  const [balance, setBalance] = useState(0)
-  const [transactions, setTransactions] = useState({
+  const [wallet, setWallet] = useState();
+  const [balance, setBalance] = useState(0);
+  const [transaction, setTransaction] = useState({
     to: "",
-    
-  })
+    value: 0,
+  });
+
+  const dummy =
+    "peasant horror box announce galaxy excess put enhance require mesh endless advice";
 
   useEffect(() => {
-    window.ethersProvider = new providers.InfuraProvider("rinkeby")
-  }, [])
+    window.ethersProvider = new providers.InfuraProvider("rinkeby");
+  }, []);
 
   useEffect(() => {
     if (wallet) {
-      window.ethersProvider.getBalance(wallet.address).then(balance => {
-        setBalance(balance)
-      })
+      window.ethersProvider.getBalance(wallet.address).then((balance) => {
+        setBalance(balance);
+      });
     }
-  }, [wallet])
+  }, [wallet]);
 
   function genreateNewMnemonic() {
     setMnemonic(generateMnemonic());
@@ -31,8 +34,24 @@ function App() {
 
   function createWalletFromMnemonic() {
     let walletCreate = Wallet.fromMnemonic(mnemonic ? mnemonic : mnemonicInput);
-    walletCreate = {...walletCreate, provider: window.ethersProvider}
-    setWallet(walletCreate)
+    setWallet(walletCreate);
+  }
+
+  function handleTransactionChange(e) {
+    setTransaction({ ...transaction, [e.target.name]: e.target.value });
+  }
+
+  async function sendTransaction() {
+    if (transaction.to && transaction.value > 0) {
+      let tx = {
+        ...transaction,
+        value: utils.parseEther(transaction.value),
+      };
+
+      await wallet.signTransaction(tx);
+      let walletInstance = wallet.connect(window.ethersProvider);
+      await walletInstance.sendTransaction(tx);
+    }
   }
 
   return (
@@ -46,7 +65,10 @@ function App() {
       ) : null}
 
       <p>Or import Wallet using Mnemonic</p>
-      <input value={mnemonicInput} onChange={(e) => setMnemonicInput(e.target.value)} />
+      <input
+        value={mnemonicInput}
+        onChange={(e) => setMnemonicInput(e.target.value)}
+      />
 
       <button onClick={createWalletFromMnemonic}>Create Wallet</button>
       <div>Your wallet information</div>
@@ -57,8 +79,19 @@ function App() {
           <div>
             <h3>Send Transaction</h3>
             <p>To: </p>
-            <input />
+            <input
+              value={transaction.to}
+              name="to"
+              onChange={handleTransactionChange}
+            />
+            <p>Value: </p>
+            <input
+              value={transaction.value}
+              name="value"
+              onChange={handleTransactionChange}
+            />
           </div>
+          <button onClick={sendTransaction}>Send</button>
         </div>
       ) : null}
     </div>
